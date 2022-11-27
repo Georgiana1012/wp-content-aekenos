52acf1d39dd73641b054551a0e878396d2710050
Georgiana1012 <giordache1012@gmail.com>
2022-11-27 12:11:22 +0200

    First Commit

---
+++ plugins/social-pug/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4b0cbdb746b038fdb458c5ff9521e5598fdfb2d1',
+        'name' => 'mediavine/mediavine-grow-pro',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kernl/kernl-update-checker' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kernl/kernl-update-checker',
+            'aliases' => array(),
+            'reference' => '84e4ec84f351bd766ae72cc1d9b27cf109382e95',
+            'dev_requirement' => false,
+        ),
+        'mediavine/mediavine-grow-pro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4b0cbdb746b038fdb458c5ff9521e5598fdfb2d1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
